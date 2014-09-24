49ca32f2ce9e8f2c1a3a87f5a7a3ab3de021a23e
Johan Bucht <johan.bucht@netent.com>
2014-09-24 15:15:26 +0200

    Add docker support

---
+++ docker/db-setup/scripts/schema.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS event;
+
+CREATE TABLE IF NOT EXISTS event.events (
+    UserId       INT                NOT NULL,
+    GameId       VARCHAR(50)       NOT NULL,
+    State        BYTEA              NOT NULL,
+    GameRoundId    BIGINT PRIMARY KEY NOT NULL,
+    Id           BIGINT             NOT NULL,
+   GameRoundData BYTEA 
+)
---
+++ docker/db/run.sh
@@ -0,0 +1,1 @@
+docker run -it --name vgs-db --rm -p 5433:5432 vgs-db
